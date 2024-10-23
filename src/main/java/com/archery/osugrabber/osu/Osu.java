9d9977824064accc2102f030306dbde74fa36169
archery <kinetix0000@gmail.com>
2024-10-23 00:38:27 -0500

    Osu now checks for valid API Key.

--- src/main/java/com/archery/osugrabber/osu/Osu.java
+++ src/main/java/com/archery/osugrabber/osu/Osu.java
@@ -1,5 +1,6 @@
 package com.archery.osugrabber.osu;
 
+import com.archery.osugrabber.enums.GameMode;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -22,6 +23,7 @@ public class Osu {
     private final String getBeatmapBase = "get_beatmaps?b=";
     private final String getBeatmapsetBase = "get_beatmaps?s=";
 
+    public boolean isValidKey = false;
 
     private final HttpClient client;
     private final Gson gson = new Gson();
@@ -29,6 +31,18 @@ public class Osu {
     public Osu(String apiKey) {
         this.apiKey = "&k=" + apiKey;
         client = HttpClient.newHttpClient();
+
+        /*
+        * Check API key works?
+        * Should find a better way lol
+        * */
+
+        try {
+            this.getUser("archeryiskey");
+            isValidKey = true;
+        } catch (Exception e) {
+            System.err.println("Please provide a valid API key.");
+        }
     }
 
     public User getUser(@NotNull String username, boolean debug) throws IOException, InterruptedException {
@@ -53,16 +67,17 @@ public class Osu {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getUserBestBase + user.getUserID() + "&limit=" + limit + apiKey)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         JsonElement bestJson = gson.fromJson(response.body(), JsonElement.class);
-        JsonArray bestArray = bestJson.getAsJsonArray();
-        List<Play> plays = new ArrayList<>();
-        bestArray.forEach(play -> {
-            try {
-                int beatmapID = play.getAsJsonObject().get("beatmap_id").getAsInt();
-                double pp = play.getAsJsonObject().get("pp").getAsDouble();
-                Beatmap beatmap = getBeatmap(beatmapID);
-                plays.add(new Play(user.getUsername(), beatmap.getArtist(), beatmap.getTitle(), beatmap.getVersion(), pp));
-            } catch (IOException | InterruptedException e) {}
-        });
+        List<Play> plays = getPlays(user, bestJson);
+
+        return plays;
+    }
+
+    public List<Play> getBest(@NotNull User user, int limit, GameMode mode) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getUserBestBase + user.getUserID() + "&limit=" + limit  + "&m=" + mode.getID() + apiKey)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JsonElement bestJson = gson.fromJson(response.body(), JsonElement.class);
+        List<Play> plays = getPlays(user, bestJson);
+
         return plays;
     }
 
@@ -70,6 +85,40 @@ public class Osu {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getUserBestBase + user.getUserID() + apiKey)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         JsonElement bestJson = gson.fromJson(response.body(), JsonElement.class);
+        List<Play> plays = getPlays(user, bestJson);
+
+        return plays;
+    }
+
+    public Play getBestIndex(@NotNull User user, int index) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getUserBestBase + user.getUserID() + "&limit=100" + apiKey)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JsonElement bestJson = gson.fromJson(response.body(), JsonElement.class);
+        JsonArray bestArray = bestJson.getAsJsonArray();
+        int beatmapID = bestArray.get(index - 1).getAsJsonObject().get("beatmap_id").getAsInt();
+        double pp = bestArray.get(index - 1).getAsJsonObject().get("pp").getAsDouble();
+        Beatmap beatmap = getBeatmap(beatmapID);
+
+        return new Play(user.getUsername(), beatmap.getArtist(), beatmap.getTitle(), beatmap.getVersion(), pp);
+    }
+
+    public Beatmap getBeatmap(int beatmapID) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getBeatmapBase + beatmapID + apiKey)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JsonElement beatmapJson = gson.fromJson(response.body(), JsonElement.class);
+
+        return gson.fromJson(beatmapJson.getAsJsonArray().get(0), Beatmap.class);
+    }
+
+    public Beatmap getBeatmapSet(int beatmapsetID) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getBeatmapsetBase + beatmapsetID + apiKey)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JsonElement beatmapJson = gson.fromJson(response.body(), JsonElement.class);
+
+        return gson.fromJson(beatmapJson, Beatmap.class);
+    }
+
+    private @NotNull List<Play> getPlays(@NotNull User user, JsonElement bestJson) {
         JsonArray bestArray = bestJson.getAsJsonArray();
         List<Play> plays = new ArrayList<>();
         bestArray.forEach(play -> {
@@ -81,20 +130,6 @@ public class Osu {
             } catch (IOException | InterruptedException e) {}
         });
         return plays;
-    }
-
-    public Beatmap getBeatmap(int beatmapID) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getBeatmapBase + beatmapID + apiKey)).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonElement beatmapJson = gson.fromJson(response.body(), JsonElement.class);
-        return gson.fromJson(beatmapJson.getAsJsonArray().get(0), Beatmap.class);
-    }
-
-    public Beatmap getBeatmapSet(int beatmapsetID) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(apiBase + getBeatmapsetBase + beatmapsetID + apiKey)).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        JsonElement beatmapJson = gson.fromJson(response.body(), JsonElement.class);
-        return gson.fromJson(beatmapJson, Beatmap.class);
     }
 
     public void setAPIKey(String apiKey) {
