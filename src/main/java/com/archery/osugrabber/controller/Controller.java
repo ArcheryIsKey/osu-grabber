644685d10dac00b86ca36b2e7eac1c1d6ea6cb62
archery <kinetix0000@gmail.com>
2024-10-23 05:01:49 -0500

    Fleshed out GUI more. Added different osu!mode sections.

--- src/main/java/com/archery/osugrabber/controller/Controller.java
+++ src/main/java/com/archery/osugrabber/controller/Controller.java
@@ -2,10 +2,16 @@ package com.archery.osugrabber.controller;
 
 import com.archery.osugrabber.enums.GameMode;
 import com.archery.osugrabber.osu.Osu;
+import com.archery.osugrabber.osu.Play;
+import com.archery.osugrabber.osu.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -13,6 +19,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -31,6 +39,14 @@ public class Controller implements Initializable {
     private AnchorPane backgroundAnchor;
     @FXML
     private Stage stage;
+    @FXML
+    private TableView<Play> osu_standard_table;
+    @FXML
+    private TableView<Play> osu_mania_table;
+    @FXML
+    private TableView<Play> osu_taiko_table;
+    @FXML
+    private TableView<Play> osu_catch_table;
 
     private String osuUsername;
 
@@ -72,12 +88,55 @@ public class Controller implements Initializable {
         username.setOnKeyPressed(keyEvent -> {
             if(keyEvent.getCode() == KeyCode.ENTER) {
                 try {
-                    osu.getBest(osu.getUser(username.getText()), 5, GameMode.MANIA).forEach(play -> System.out.println(play.getFull_name() + " | " + play.getPp() + "\n"));
+
+                    User user = osu.getUser(username.getText());
+
+                    // TODO: Make this programmatic?
+
+                    ObservableList<Play> standard = FXCollections.observableArrayList(osu.getBest(user, 5, GameMode.STANDARD));
+                    ObservableList<Play> mania = FXCollections.observableArrayList(osu.getBest(user, 5, GameMode.MANIA));
+                    ObservableList<Play> taiko = FXCollections.observableArrayList(osu.getBest(user, 5, GameMode.TAIKO));
+                    ObservableList<Play> ctb = FXCollections.observableArrayList(osu.getBest(user, 5, GameMode.CATCH));
+
+                    getTables().get(0).setItems(standard);
+                    getTables().get(1).setItems(mania);
+                    getTables().get(2).setItems(taiko);
+                    getTables().get(3).setItems(ctb);
+
+                    h_box.getChildren().remove(username);
+                    h_box.getChildren().add(newTabPane);
+
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
                 }
+
+                // fuck this is ugly
+
+                osu_standard_table.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("full_name"));
+                osu_standard_table.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("pp"));
+
+                osu_mania_table.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("full_name"));
+                osu_mania_table.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("pp"));
+
+                osu_taiko_table.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("full_name"));
+                osu_taiko_table.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("pp"));
+
+                osu_catch_table.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("full_name"));
+                osu_catch_table.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("pp"));
+
+
+
             }
         });
+    }
+
+    public List<TableView> getTables() {
+        List<TableView> list = new ArrayList<>();
+        list.add(osu_standard_table);
+        list.add(osu_mania_table);
+        list.add(osu_taiko_table);
+        list.add(osu_catch_table);
+        return list;
     }
 
     public void setStage(Stage stage) {
